c98afb067a221f89d80f2de4f7add63129a27117
trevismurithi <trevismurithi@gmail.com>
2021-01-18 22:37:51 +0300

    make not readable

--- index.php
+++ index.php
@@ -7,23 +7,5 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="includes/signup.inc.php" method="post">
-        <input type="text" name="first" id="">
-        <input type="text" name="last" id="">
-        <input type="text" name="email" id="">
-        <input type="text" name="uid" id="">
-        <input type="password" name="pwd" id="">
-        <button type="submit">Sign Up</button>
-    </form>
-    <?php
-        //get the full url
-        $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        
-        if(strpos($fullUrl,"signup=empty")==true){
-            echo "You did not fill all fields";
-        }elseif (strpos($fullUrl,"signup=invalidemail")==true) {
-             echo "you used invalid email";
-        }
-    ?>
 </body>
 </html>
