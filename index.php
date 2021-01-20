7a554a8fdb18da7f72f8fb080264e42485ac26b2
trevismurithi <trevismurithi@gmail.com>
2021-01-20 10:31:03 +0300

    dehash and hash

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+</head>
 <body>
+    <?php
+        // echo "test123<br>";
+        // echo password_hash("test123",PASSWORD_DEFAULT);
+
+        $input  = "test123";
+        $hashed = password_hash("test123",PASSWORD_DEFAULT);
+        echo password_verify($input,$hashed);
+
+    ?>
 </body>
+</html>
 </html>
